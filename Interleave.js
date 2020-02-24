40887d7063fd7234581d66a611076adc3092ce2e
Tejaswini-Nalla <tejaswini.nalla@thoughtworks.com>
2020-02-25 02:27:49 +0530

    Add interleave function in Interleave.js

--- Interleave.js
+++ Interleave.js
@@ -1,3 +1,13 @@
+function interleave(text, stringToBeInserted) {
+    let letters = text.split("");
+    return letters.join(stringToBeInserted);
+}
+
 function main() {
     console.log(interleave("abc", ","));
+    console.log(interleave("1234", "e&a"));
+    console.log(interleave("", "x"));
+    console.log(interleave(" 123", ","));
 }
+
+main()
