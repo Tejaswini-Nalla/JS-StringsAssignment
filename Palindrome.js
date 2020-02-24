805ccc0196407ff14202c16d12e22f104ded8ee0
Tejaswini-Nalla <tejaswini.nalla@thoughtworks.com>
2020-02-25 01:12:59 +0530

    Add isPalindrome function

--- Palindrome.js
+++ Palindrome.js
@@ -1,7 +1,19 @@
-function isPalindrome(string) {
+function areSameCharacters(character1, character2) {
+    return character1 == character2;
+}
 
+function isPalindrome(string) {
+    for(let index = 0; index < string.length/2; index++) {
+        let indexFromLast = string.length - 1 - index;
+        if(!areSameCharacters(string[index], string[indexFromLast])) {
+            return false;
+        }
+    }
+    return true;
 }
 
 function main() {
     console.log("Is madam Palindrome or not ?",isPalindrome("madam"));
 }
+
+main()
