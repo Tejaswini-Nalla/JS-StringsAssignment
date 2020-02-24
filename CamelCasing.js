32228737834845a56919dc0e95858f1846b7ede7
Tejaswini-Nalla <tejaswini.nalla@thoughtworks.com>
2020-02-25 01:32:07 +0530

    Add function camelCase in CamelCAsing.js

--- CamelCasing.js
+++ CamelCasing.js
@@ -1,3 +1,15 @@
+function getTitleCase(word) {
+
+}
+
+function camelCase(words) {
+    let result = words[0].toLowerCase();
+    for(let index = 1; index < words.length; index++) {
+        let titleCaseWord = getTitleCase(words[index]);
+        result = result.concat()
+    }
+}
+
 function main() {
     console.log("CamelCase Notation of words ['hi','good', 'morning']",camelCase(['hi','good', 'morning']));
 }
