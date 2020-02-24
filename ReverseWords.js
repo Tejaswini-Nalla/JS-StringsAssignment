71c8487043965ba9a411888b1b571befa5f34d15
Tejaswini-Nalla <tejaswini.nalla@thoughtworks.com>
2020-02-25 02:16:10 +0530

    Add reverseWords function in ReverseWords.js

--- ReverseWords.js
+++ ReverseWords.js
@@ -1,3 +1,21 @@
+function reverse(string) {
+    let reversedString = "";
+        for(let index = string.length - 1; index >= 0; index--) {
+            reversedString = reversedString + string[index];
+        }
+    return reversedString;
+}
+
+function reverseWords(string) {
+    let words = string.split(" ");
+    console.log(words);
+    let reversedWords = [];
+    for(let index = 0; index < words.length; index++) {
+        reversedWords = reversedWords.concat(reverse(words[index]));
+    }
+    return reversedWords.join(" ");
+}
+
 function main() {
     console.log("String 'hello world' After reversing words",reverseWords("hello world"));
 }
