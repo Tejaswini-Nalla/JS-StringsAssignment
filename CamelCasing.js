efff714d5d2ad68160b8eb9429cffd1bc8e71c6c
Tejaswini-Nalla <tejaswini.nalla@thoughtworks.com>
2020-02-25 01:34:22 +0530

    Add getTitleCase function in CamelCasing.js

--- CamelCasing.js
+++ CamelCasing.js
@@ -1,13 +1,14 @@
 function getTitleCase(word) {
-
+    return word[0].toUpperCase().concat(word.slice(1));
 }
 
 function camelCase(words) {
     let result = words[0].toLowerCase();
     for(let index = 1; index < words.length; index++) {
         let titleCaseWord = getTitleCase(words[index]);
-        result = result.concat()
+        result = result.concat(titleCaseWord);
     }
+    return result;
 }
 
 function main() {
