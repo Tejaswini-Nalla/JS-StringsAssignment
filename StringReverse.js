1a9696957fdc2780c97b244b88b54346bf740614
Tejaswini-Nalla <tejaswini.nalla@thoughtworks.com>
2020-02-25 00:59:11 +0530

    Add reverse function in StringReverse.js

---
+++ StringReverse.js
@@ -0,0 +1,14 @@
+function reverse(string) {
+    let reversedString = "";
+    for(let index = string.length - 1; index >= 0; index--) {
+        reversedString = reversedString + string[index];
+    }
+    return reversedString;
+}
+
+function main() {
+    console.log("Reverse of 'Even this should be in reverse':",reverse("Even this should be in reverse"));
+    console.log("Reverse of 'hello':",reverse("hello"));
+}
+
+main()
