90ffc91e3f3b7241681de6a4358cdaf7400abfa3
Tejaswini-Nalla <tejaswini.nalla@thoughtworks.com>
2020-02-25 12:30:18 +0530

    Add strip function in Strip.js

--- Strip.js
+++ Strip.js
@@ -1,4 +1,16 @@
+function isSpace(word) {
+    return word != " ";
+}
+function strip(sentence) {
+    let words = sentence.split("");
+    let strippedSentence = words.filter(isSpace).join("");
+    return strippedSentence;
+}
+
 function main() {
     console.log(strip(" hello "));
     console.log(strip(" hello"));
+    console.log(strip("  hello "));
 }
+
+main();
