5398a99997d9bb6cbb97d7d8556156213757b602
Tejaswini-Nalla <tejaswini.nalla@thoughtworks.com>
2020-02-25 11:45:05 +0530

    Add areSameWords function in WordFrequency.js

--- WordFrequency.js
+++ WordFrequency.js
@@ -1,10 +1,15 @@
+function areSameWords(word1, word2) {
+    return word1 === word2;
+}
 function wordFrequency(text, word) {
+    let count = 0;
     let wordsInText = text.split(" ");
     for(let index=0; index < wordsInText.length; index++) {
         if(areSameWords(wordsInText[index],word)) {
-
+            count++;
         }
     }
+    return count;
 }
 
 function main() {
