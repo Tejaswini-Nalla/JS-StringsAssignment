e6b2af69ac7fa12352fabc1864d2277a9aa01c42
Tejaswini-Nalla <tejaswini.nalla@thoughtworks.com>
2020-02-26 14:24:48 +0530

    Modify strip function in Strip.js

--- CamelCasing.js
+++ CamelCasing.js
@@ -15,4 +15,4 @@ function main() {
     console.log("CamelCase Notation of words ['hi','good', 'morning']",camelCase(['hi','good', 'morning']));
 }
 
-main()
+main();
--- Strip.js
+++ Strip.js
@@ -1,16 +1,28 @@
-function isSpace(word) {
+/*function isSpace(word) {
     return word != " ";
+}*/
+
+function removeSpaces(list) {
+    let index = list.length-1;
+    while(list[index] === " ") {
+        list.pop();
+        index--;
+    }
+    return list;
 }
+
 function strip(sentence) {
     let words = sentence.split("");
-    let strippedSentence = words.filter(isSpace).join("");
-    return strippedSentence;
+    words = removeSpaces(words);
+    words = removeSpaces(words.reverse());
+    return words.join("");
 }
 
 function main() {
     console.log(strip(" hello "));
     console.log(strip(" hello"));
     console.log(strip("  hello "));
+    console.log(strip("  hello world "));
 }
 
 main();
